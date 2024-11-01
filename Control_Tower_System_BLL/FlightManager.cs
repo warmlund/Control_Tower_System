746811c423b43e4e9354d4e19d267b831e685d5e
warmlund <emeliewarmlund@gmail.com>
2024-11-01 13:21:03 +0100

    Set up flight manager class with event delegates for takeoff and landing

--- Control_Tower_System_BLL/FlightManager.cs
+++ Control_Tower_System_BLL/FlightManager.cs
@@ -1,14 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Control_Tower_System_DTO;
+﻿using Control_Tower_System_DTO;
 
 namespace Control_Tower_System_BLL
 {
+    /// <summary>
+    /// This class handles two delegate events for flight takeoff and landing
+    /// "Blueprint" for Flight is in the flight class in the DTO layer
+    /// </summary>
     internal class FlightManager
     {
-       public delegate void 
+        public event EventHandler<FlightTakeOffEventArgs> TakingOff;
+        public event EventHandler<FlightLandedEventArgs> Landing;
+
+        public void TakeOff(Flight flight, DateTime time) => OnTakeOff(flight, time);
+        public void Land(Flight flight, DateTime time) => OnLanding(flight, time);
+
+        private void OnTakeOff(Flight flight, DateTime takeOffTime)
+        {
+            TakingOff?.Invoke(flight, new FlightTakeOffEventArgs(flight, takeOffTime));
+        }
+
+        private void OnLanding(Flight flight, DateTime landingTime)
+        {
+            Landing?.Invoke(flight, new FlightLandedEventArgs(flight, landingTime));
+        }
     }
 }
