37c07d0048258c7824eafcd22d731f49ff91aceb
warmlund <emeliewarmlund@gmail.com>
2024-11-03 10:47:20 +0100

    Setup testing class for BLL layer

---
+++ Control_Towe_System_BLL_Tests/ControlTowerTest.cs
@@ -0,0 +1,17 @@
+namespace Control_Towe_System_BLL_Tests
+{
+    [TestClass]
+    public class ControlTowerTest
+    {
+        [TestMethod]
+        public void ChangeAltitudeTest()
+        {
+        }
+
+        [TestMethod]
+        public void CreateFlightTest()
+        {
+
+        }
+    }
+}
---
+++ Control_Towe_System_BLL_Tests/FlightManagerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_Towe_System_BLL_Tests
+{
+    [TestClass]
+    public class FlightManagerTest
+    {
+        [TestMethod]
+        public void OnTakeOffTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void OnLandingTest()
+        {
+
+        }
+    }
+}
