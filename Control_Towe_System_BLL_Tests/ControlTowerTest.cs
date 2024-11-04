74848a5901d57f1623f72c41bb3b90b13d7eed1d
warmlund <emeliewarmlund@gmail.com>
2024-11-04 11:05:52 +0100

    Commenting and documentation

--- Control_Towe_System_BLL_Tests/ControlTowerTest.cs
+++ Control_Towe_System_BLL_Tests/ControlTowerTest.cs
@@ -1,4 +1,6 @@
 using Control_Tower_System_BLL;
+using Control_Tower_System_DTO;
+using System.Data.Common;
 
 namespace Control_Tower_System_BLL_Tests
 {
@@ -8,26 +10,32 @@ namespace Control_Tower_System_BLL_Tests
         /// <summary>
         /// Sets private instance variables used in both test methods
         /// </summary>
-        private const string testFlightId = "FlightId";
-        private const string testAirline = "Airline";
-        private const string testDestination = "Destination";
-        private const double testDuration = 1;
         private const double testAltitude = 5000;
         private const int testFlightIndex = 0;
         private ControlTower controlTower;
+
+        /// <summary>
+        /// Creates a test initialize class
+        /// creates a instance variable of the flightmanager class
+        /// and creates a test flight
+        /// These are used for all test methods
+        /// </summary>
+        [TestInitialize]
+        public void Setup()
+        {
+            controlTower = new ControlTower();
+            controlTower.CreateFlight("TesFlightId", "TestAirline", "TestDestination", 1.0);
+        }
+
 
         [TestMethod]
         public void ChangeAltitudeTest()
         {
             //Arrange 
-            //creates instance of controlTower and creates a testflight
-            controlTower = new ControlTower();
-            controlTower.CreateFlight(testFlightId, testAirline, testDestination, testDuration);
-
             bool isEventTriggered = false; //Sets bool for tracking event trigger to false
             FlightHeightEventArgs heightEventArgs = null; //sets FlightHeightEventArgs variable to null
 
-            controlTower.FlightAltitudeChanged += (s, e) => //Sets event handler to flightAltitudeChanged event
+            controlTower.FlightAltitudeChanged += (s, e) => //Attach event handler to flightAltitudeChanged event
             {
                 isEventTriggered = true; //if the event is invoked in the test method, this will set to true
                 heightEventArgs = e; //if the event is invoked in the test method, the eventargs variable will be set to the eventarg in the eventhandler
@@ -47,16 +55,15 @@ namespace Control_Tower_System_BLL_Tests
             //Arrange
             //Creates instance of the Control tower class
             //Gets the current amount of flights in the flightstorage
-            controlTower = new ControlTower();
             int numberOfFlightsInStorage = controlTower.FStorage.Count;
 
             //Act
             //Calls the createflight class
-            controlTower.CreateFlight(testFlightId, testAirline, testDestination, testDuration);
+            controlTower.CreateFlight("TestId", "TestAirline", "TestDestination", 2.0);
 
             //Assert
             //Asserts if the original storage count is the same as the new count after the method has been called minus 1
-            Assert.IsTrue(numberOfFlightsInStorage == controlTower.FStorage.Count - 1, "Flight is not added to storage"); 
+            Assert.IsTrue(numberOfFlightsInStorage == controlTower.FStorage.Count - 1, "Flight is not added to storage");
         }
     }
 }
